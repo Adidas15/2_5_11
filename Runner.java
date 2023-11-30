ec875e61ee30ddb192d24f6fdf0f8f1428b1d11e
Adidas15 <adidasglhf@gmail.com>
2023-11-30 14:03:30 -0800

    Setting up playing

--- Runner.java
+++ Runner.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
 public class Runner {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Table table = new Table();
         Player p1 = new Player("Aditya");
         Player p2 = new Player("Jonathan");
+        int pi;
         table.setup();
         System.out.println(table.getPieces());
+        while (table.getPieces()>1) {
+            System.out.println("Enter pieces: ");
+            pi = sc.nextInt();
+            
+        }
+        
     }
 }
--- Table.java
+++ Table.java
@@ -1,21 +1,17 @@
 import java.lang.Math;
 public class Table {
-    private int pieces;
+    private int nims = 10 + (int) (Math.random() * 41);
     private int turns;
 
-    public void setup() {
-        pieces = 10 + (int) (Math.random() * 41);
-    }
-
-    public void removePieces(int num) {
-        if (num < pieces/2 && pieces >=1) {
-            pieces -= num;
+    public boolean chanegBoard(int num) {
+        if (num < nims/2 && nims >=1) {
+            nims -= num;
             turns +=1;
+            return true;
         }
-    }
-
-    public int getPieces() {
-        return pieces;
+        else {
+            return false;
+        }
     }
 
 }
