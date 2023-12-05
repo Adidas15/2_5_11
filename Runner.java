34e987e749f3b8297594023945413b1a9625a035
Yee-Dino <142348984+Yee-Dino@users.noreply.github.com>
2023-12-04 18:02:15 -0800

    added comments

--- Runner.java
+++ Runner.java
@@ -1,50 +1,50 @@
-import java.util.Scanner;
+import java.util.Scanner; //import libraries
 import java.lang.Math;
 public class Runner {
 
     public static void main(String[] args) {
         
-            Scanner sc = new Scanner(System.in);
+            Scanner sc = new Scanner(System.in); 
             Table table = new Table();
             
-            System.out.println("Enter player one's name: ");
-            String p1Name = sc.nextLine();
-            Player p1 = new Player(p1Name);
-            System.out.println("Enter player two's name: ");
-            String p2Name = sc.nextLine();
-            Player p2 = new Player(p2Name);
+            System.out.println("Enter player one's name: "); //gets player 1 name
+            String p1Name = sc.nextLine(); //takes player 1's name as an input
+            Player p1 = new Player(p1Name); //creates player 1 object
+            System.out.println("Enter player two's name: "); //gets player 2 name
+            String p2Name = sc.nextLine();//takes player 2's name as an input
+            Player p2 = new Player(p2Name); //creates player 2 object
             Player currentPlayer;
             int pi;
             String play = "y";
-            while (play.equals("y")) {
+            while (play.equals("y")) { //program loop
                 table.setNims();
-                p1.resetPoints();
+                p1.resetPoints(); //replay
                 p2.resetPoints();
-                if (Math.random()>=0.50) {
+                if (Math.random()>=0.50) { //sets which player goes first
                     currentPlayer = p1;
                 }
                 else {
                     currentPlayer = p2;
                 }
-                while (table.getNims()>1) {
-                    System.out.println(currentPlayer.toString());
-                    System.out.println("There are " + table.getNims() + " nims on the board.");
-                    System.out.println("Enter nims: ");
-                    pi = sc.nextInt();
-                    if (table.changeBoard(pi)) {
-                        currentPlayer.addPoints(pi);
+                while (table.getNims()>1) { //game loop
+                    System.out.println(currentPlayer.toString()); //prints out which player's turn it is
+                    System.out.println("There are " + table.getNims() + " nims on the board."); //prints out # of nims
+                    System.out.println("Enter nims: "); 
+                    pi = sc.nextInt(); //takes player input
+                    if (table.changeBoard(pi)) { //checks if the player input is valid
+                        currentPlayer.addPoints(pi); //if it is, add their points
                     }
                     else {
-                        System.out.println("Invalid number of nims. Try again: ");
-                        pi = sc.nextInt();
+                        System.out.println("Invalid number of nims. Try again: "); //if it isn't, ask for a new input
+                        pi = sc.nextInt(); 
                         if (table.changeBoard(pi)) {
                             currentPlayer.addPoints(pi);
                         }
                         else {
-                            System.out.println("Too many incorrect guesses. You have forfeited your turn. Moving on...");
+                            System.out.println("Too many incorrect guesses. You have forfeited your turn. Moving on..."); //so people actually play the game
                         }
                     }     
-                    if (currentPlayer.equals(p1)) {
+                    if (currentPlayer.equals(p1)) { //changes turn
                         currentPlayer = p2;
                     }       
                     else {
@@ -52,24 +52,24 @@ public class Runner {
                     }
                 }
                 
-                System.out.print(currentPlayer.getName() + " is the loser! ");
-                System.out.print(currentPlayer.getName() + " had " + currentPlayer.getPoints() + " points. ");
-                System.out.println(currentPlayer.getName() + "'s wins so far: " + currentPlayer.getWins());
-                if (currentPlayer.equals(p1)) {
+                System.out.print(currentPlayer.getName() + " is the loser! "); //prints who lost
+                System.out.print(currentPlayer.getName() + " had " + currentPlayer.getPoints() + " points. "); //prints who won
+                System.out.println(currentPlayer.getName() + "'s wins so far: " + currentPlayer.getWins()); //prints total wins for player 1
+                if (currentPlayer.equals(p1)) { //changes turn per game
                     currentPlayer = p2;
                 }       
                 else {
                     currentPlayer = p1;
                 }
-                currentPlayer.addWin();
-                System.out.print(currentPlayer.getName() + " is the winner! ");
-                System.out.print(currentPlayer.getName() + " had " + currentPlayer.getPoints() + " points. ");
-                System.out.println(currentPlayer.getName() + "'s wins so far: " + currentPlayer.getWins());
+                currentPlayer.addWin(); //counts up wins
+                System.out.print(currentPlayer.getName() + " is the winner!  "); //prints out wins
+                System.out.print(currentPlayer.getName() + " had " + currentPlayer.getPoints() + " points. "); //prints our total scorecard
+                System.out.println(currentPlayer.getName() + "'s wins so far: " + currentPlayer.getWins()); //prints out other player's wins
 
-                System.out.println("Play again? (y/n)");
+                System.out.println("Play again? (y/n)"); //replay?
                 play = sc.next();
             }
-            System.out.println("Game ended");
+            System.out.println("Game ended");//if no, game ends
 
     }
     
--- Table.java
+++ Table.java
@@ -3,21 +3,21 @@ public class Table {
     private int nims;
 
     public boolean changeBoard(int num) {
-        if (num <= nims/2 && num >=1) {
-            nims -= num;
-            return true;
+        if (num <= nims/2 && num >=1) { //checks to make sure that conditions for a move are met, which are that it has to be greater than 0 and less than half the remianing nims.
+            nims -= num; //reduces total amount of nimes
+            return true; //makes the turn go
         }
         else {
-            return false;
+            return false; //returns false so it asks again
         }
     }
 
-    public void setNims() {
+    public void setNims() { //sets the starting # of nims
         nims = 10 + (int) (Math.random() * 41);
     }
 
     public int getNims() {
-        return nims;
+        return nims; //returns amount of nims left
     }
 
 }
